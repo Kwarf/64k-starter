36427e9e6b478722832ff790abe646df5a41bf8a
Jimmy Bergström <me@kwarf.com>
2024-06-21 01:08:11 +0200

    Use ptr::addr_of_mut! to remove warnings

--- src/time.rs
+++ src/time.rs
@@ -1,17 +1,17 @@
-use core::time::Duration;
+use core::{ptr, time::Duration};
 
 use windows_sys::Win32::System::Performance::{QueryPerformanceCounter, QueryPerformanceFrequency};
 
 unsafe fn ticks() -> i64 {
     let mut count = 0i64;
-    QueryPerformanceCounter(&mut count as *mut i64);
+    QueryPerformanceCounter(ptr::addr_of_mut!(count));
     count
 }
 
 pub unsafe fn elapsed() -> Duration {
     static mut FREQ: i64 = 0;
     if FREQ == 0 {
-        QueryPerformanceFrequency(&mut FREQ as *mut i64);
+        QueryPerformanceFrequency(ptr::addr_of_mut!(FREQ));
     }
 
     static mut START: i64 = 0;
